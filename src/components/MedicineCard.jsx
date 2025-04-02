00896e424fdc72d14bdf084f818fdbfa44b3f709
Jay Gangani <Jay Gangani>
2025-04-02 22:30:21 +0530

    latest bug fix

--- src/components/MedicineCard.jsx
+++ src/components/MedicineCard.jsx
@@ -12,24 +12,20 @@ const MedicineCard = (props) => {
 
   useEffect(() => {
     firebase.getImageURL(props.imageURL).then((url) => setURL(url));
-  }, []);
+  }, [firebase, props.imageURL]);
 
   return (
-    <div class=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md h-full d-flex justify-content-between align-items-center flex-column">
+    <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow-md h-full d-flex justify-content-between align-items-center flex-column">
       <a href="#" className="d-flex justify-content-center align-items-center">
-        <img class="p-1 rounded-t-lg" src={url} alt="product image" />
+        <img className="p-1 rounded-t-lg" src={url} alt="product image" />
       </a>
-      <div class="px-4 pb-3">
-        <h5 class="text-xl font-semibold tracking-tight text-black-900 ">
-          {props.name}
-        </h5>
-        <div class="flex items-center justify-center">
-          <span class="text-2xl font-bold text-black-900 me-3">
-            ₹{props.price}
-          </span>
+      <div className="px-4 pb-3">
+        <h5 className="text-xl font-semibold tracking-tight text-black-900 ">{props.name}</h5>
+        <div className="flex items-center justify-center">
+          <span className="text-2xl font-bold text-black-900 me-3">₹{props.price}</span>
           <button
             onClick={(e) => navigate(props.link)}
-            class="text-white bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="text-white bg-gray-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             View
           </button>
